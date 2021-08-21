92ceefa3abb38c954b7defa37c840f481d4185d9
Feroj7 <feroj.web@gmail.com>
2021-08-21 20:19:08 +0600

    organize code

--- js/app.js
+++ js/app.js
@@ -1,48 +1,37 @@
 // memory button event handler and extra memory cost update
-const memoryButton1 = document.getElementById('memory-btn1');
-memoryButton1.addEventListener('click', function(){
-    const memoryPrice = document.getElementById('memory-price');
+const memoryPrice = document.getElementById('memory-price');
+document.getElementById('memory-btn1').addEventListener('click', function(){
     memoryPrice.innerText = '0';
     calculateTotal();
 })
-const memoryButton2 = document.getElementById('memory-btn2');
-memoryButton2.addEventListener('click', function(){
-    const memoryPrice = document.getElementById('memory-price');
+document.getElementById('memory-btn2').addEventListener('click', function(){
     memoryPrice.innerText = '180';
     calculateTotal();
 })
 
 //storage button event handler and extra storage cost update
-const storageButton1 = document.getElementById('storage-btn1');
-storageButton1.addEventListener('click', function(){
-    const storagePrice = document.getElementById('storage-price');
+const storagePrice = document.getElementById('storage-price');
+document.getElementById('storage-btn1').addEventListener('click', function(){
     storagePrice.innerText = '0';
     calculateTotal();
 })
-const storageButton2 = document.getElementById('storage-btn2');
-storageButton2.addEventListener('click', function(){
-    const storagePrice = document.getElementById('storage-price');
+document.getElementById('storage-btn2').addEventListener('click', function(){
     storagePrice.innerText = '100';
     calculateTotal();
 })
-const storageButton3 = document.getElementById('storage-btn3');
-storageButton3.addEventListener('click', function(){
-    const storagePrice = document.getElementById('storage-price');
+document.getElementById('storage-btn3').addEventListener('click', function(){
     storagePrice.innerText = '180';
     calculateTotal();
 })
 
-//delivery button event handler and delivery cost update
-const deliveryButton1 = document.getElementById('delivery-btn1');
-deliveryButton1.addEventListener('click', function(){
-    const deliveryPrice = document.getElementById('delivery-price');
-    deliveryPrice.innerText = '0';
+// delivery button event handler and delivery cost update
+const deliveryCost = document.getElementById('delivery-cost');
+document.getElementById('delivery-btn1').addEventListener('click', function(){
+    deliveryCost.innerText = '0';
     calculateTotal();
 })
-const deliveryButton2 = document.getElementById('delivery-btn2');
-deliveryButton2.addEventListener('click', function(){
-    const deliveryPrice = document.getElementById('delivery-price');
-    deliveryPrice.innerText = '20';
+document.getElementById('delivery-btn2').addEventListener('click', function(){
+    deliveryCost.innerText = '20';
     calculateTotal();
 })
 
@@ -51,7 +40,7 @@ function calculateTotal(){
     const bestPrice = document.getElementById('best-price').innerText;
     const memoryPrice = document.getElementById('memory-price').innerText;
     const storagePrice = document.getElementById('storage-price').innerText;
-    const deliveryCharge = document.getElementById('delivery-price').innerText;
+    const deliveryCharge = document.getElementById('delivery-cost').innerText;
     //calculate total cost
     const totalCost = parseInt(bestPrice) + parseInt(memoryPrice) + parseInt(storagePrice) + parseInt(deliveryCharge);
     //update in UI
@@ -60,6 +49,7 @@ function calculateTotal(){
     const grandTotal = document.getElementById('grand-total');
     grandTotal.innerText = totalCost;
 }
+
 //promo code apply function and give discount 
 function applyPromoCode(){
     const inputField = document.getElementById('input-field');
@@ -69,7 +59,7 @@ function applyPromoCode(){
     if(inputField.value == 'stevekaku'){
         grandTotal.innerText = totalPrice.innerText - parseFloat(totalPrice.innerText * (20/100));
     }
-    //clear promo input field
+    //clear promo code input field
     inputField.value = '';
 }
 
